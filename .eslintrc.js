678cb9633ecfa8e76e97dac7b6e69448d222fe94
Simo Mäkelä <1053209+makes@users.noreply.github.com>
2019-04-07 11:34:11 +0300

    More ES6 style fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
     "rules": {
         "indent": ["error", 4],
         "no-console": "off",
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
         "prefer-destructuring": "off",
         "no-param-reassign": [2, { "props": false }],
         "no-underscore-dangle": ["error", { "allow": ["_id"] }]
     },
     "env": {
         "browser": true,
-        "node": true
+        "node": true,
+        "jquery": true
     }
 };
