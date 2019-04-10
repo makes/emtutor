a547fafa759b88f0c5fd171ea2f122a3853b7077
Simo Mäkelä <1053209+makes@users.noreply.github.com>
2019-04-10 19:56:24 +0300

    Created modal for question feedback.

--- client/views/question.js
+++ client/views/question.js
@@ -1,6 +1,6 @@
 const m = require('mithril');
 
-const question = require('../models/question');
+const question = require('../models/question.js');
 const ecg = require('../lib/ecg.js');
 
 function resizeEcg(vnode) {
@@ -13,23 +13,57 @@ function animateEcg(vnode) {
     window.addEventListener('resize', () => { resizeEcg(vnode); }, false);
 }
 
+const feedbackModal = {
+    content: '',
+    correctAnswer: Boolean,
+    isVisible: () => feedbackModal.content !== '',
+    setContent: (text) => { feedbackModal.content = text; },
+    view: vnode => m('.modal.fade[tabindex=-1][role=dialog]',
+        [
+            m('.modal-dialog[role=document]', [
+                m('.modal-content', [
+                    m('.modal-header', [
+                        m('h5.modal-title', vnode.attrs.title || 'Are you sure?'),
+                    ]),
+                    m('.modal-body', [
+                        m('div', vnode.attrs.body || ''),
+                    ]),
+                    m('.modal-footer', vnode.attrs.buttons),
+                ]),
+            ]),
+        ]),
+    oncreate: (vnode) => {
+        $(vnode.dom).modal('show');
+    },
+};
+
 function view(vnode) {
     if (vnode.state.loading) {
         return m('h3', 'Loading...');
     }
     const currentQuiz = vnode.attrs.quiz;
     const nextQuestion = parseInt(vnode.attrs.question, 10) + 1;
-    return [
-        m('div', { id: 'ecg', class: 'container-fluid' },
-            m('canvas', {
-                id: 'chart', height: '250', oncreate: animateEcg, onupdate: resizeEcg,
-            })),
-        [
-            m('h3', question.current.title),
-            question.current.choices.map(choice => m('button', {
-                class: 'btn btn-danger btn-lg btn-block', onClick: `window.location = '/question/${currentQuiz}/${nextQuestion}'`,
-            }, choice.title)),
-        ],
+    return [m('div', { id: 'ecg', class: 'container-fluid' }, [
+        m('canvas', {
+            id: 'chart', height: '250', oncreate: animateEcg, onupdate: resizeEcg,
+        }),
+        m('h3', question.current.title),
+        question.current.choices.map(choice => m('button', {
+            class: 'btn btn-danger btn-lg btn-block',
+            onclick: () => feedbackModal.setContent(choice.feedback),
+        }, choice.title)),
+    ]),
+    feedbackModal.isVisible()
+        ? m(feedbackModal, {
+            title: 'Otsikkoteksti',
+            body: feedbackModal.content,
+            buttons: [
+                m('button.btn.btn-primary[type=button]',
+                    { onClick: `window.location = '/question/${currentQuiz}/${nextQuestion}'` },
+                    'Seuraava'),
+            ],
+        })
+        : null,
     ];
 }
 
